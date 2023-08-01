82f472fedea275a4d57946d9b9bebe1c6ed6d05c
saymaheshdave <saymaheshdave@gmail.com>
2023-08-01 23:35:57 +0530

    Update streamlit_app.py
    Typo Fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,4 +11,4 @@ st.text('🥑🍞 Avocado Toast')
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-sl.dataframe(my_fruit_list)
+st.dataframe(my_fruit_list)
