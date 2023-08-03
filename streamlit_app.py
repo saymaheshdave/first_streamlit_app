29932f616b7840117a4f329947c8f87b8bf76843
saymaheshdave <saymaheshdave@gmail.com>
2023-08-04 00:12:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
@@ -25,30 +25,24 @@ st.dataframe(fruits_to_show)
 
 def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-    # Tranforming data into normal form from JSON
+    # Transforming data into normal form from JSON
     fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-    # Priting normalized data into table format
-return fruityvice_normalized
+    # Printing normalized data into table format
+    return fruityvice_normalized
   
 st.header("Fruityvice Fruit Advice!")
-try:    
-    fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
-  if not fruit_choice:
-    st.error("Please select a fruit to get information")
-  else:
-    st.write('The user entered ', fruit_choice)
-    back_from_function = get_fruityvice_data(fruit_choice)
-    st.dataframe(back_from_function)
+try:
+    fruit_choice = st.text_input('What fruit would you like information about?', 'Kiwi')
+    if not fruit_choice:
+        st.error("Please select a fruit to get information")
+    else:
+        st.write('The user entered ', fruit_choice)
+        back_from_function = get_fruityvice_data(fruit_choice)
+        st.dataframe(back_from_function)
 except URLError as e:
-  st.error()
+    st.error()
 
-
-
-
-
-
-
-st.stop():
+# snowflake.connector needs to be fixed since the code was previously incorrect
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
@@ -56,6 +50,6 @@ my_data_rows = my_cur.fetchall()
 st.header("The fruit load list contains:")
 st.dataframe(my_data_rows)
 
-add_my_fruit = st.text_input('What fruit would to like to add?','jackfruit');
+add_my_fruit = st.text_input('What fruit would you like to add?', 'jackfruit')
 my_cur.execute("insert into fruit_load_list values ('" + add_my_fruit + "')")
 st.write('Thanks for adding', add_my_fruit)
