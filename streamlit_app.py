f735b44aa9c0a6bf3ecd76252004b6cb070548c2
saymaheshdave <saymaheshdave@gmail.com>
2023-08-04 00:21:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,22 +45,19 @@ except URLError as e:
 
 # snowflake.connector needs to be fixed since the code was previously incorrect
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_rows = my_cur.fetchall()
 st.header("The fruit load list contains:")
-st.dataframe(my_data_rows)
 #Snowflake-related functions
 def get_fruit_load_list():
-        with my_cnx.cursor() as my_cur:
-             my_cur.execute("select * from fruit_load_list")
+        with my_cnx.cursor() as my_cur
+              my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
              return my_cur.fetchall()
 #Add a button to load gthe fruit
 if st.button('Get Fruit Load List'):
         my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
-    my_data_rows = get_fruit_load_list()
-    st.dataframe(my_data_rows)
+        my_data_rows = get_fruit_load_list()
+        st.dataframe(my_data_rows)
 
+st.stop();
 add_my_fruit = st.text_input('What fruit would you like to add?', 'jackfruit')
 my_cur.execute("insert into fruit_load_list values ('" + add_my_fruit + "')")
 st.write('Thanks for adding', add_my_fruit)
